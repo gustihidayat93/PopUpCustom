19e9782b503701e6948741bb9df23b7a1fcb8b83
Ari Munandar <arimunandar.dev@gmail.com>
2019-08-07 21:10:25 +0700

    Initial Commit

---
+++ PopUpcustom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PopUpcustom
+//
+//  Created by Ari Munandar on 07/08/19.
+//  Copyright © 2019 Gusti Hidayat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
